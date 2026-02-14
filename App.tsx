964b50c6a35c5f5377e46595a8bdfa9897d66cf8
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2026-02-14 12:05:13 +0530

    code commite for the network logger

--- App.tsx
+++ App.tsx
@@ -43,7 +43,7 @@ function App() {
               translucent={true}
             />
             <RootNavigator />
-            <NetworkLoggerButton />
+            {/* <NetworkLoggerButton /> */}
           </SafeAreaProvider>
         </SidebarProvider>
       </PersistGate>
